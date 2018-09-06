1353ef608b49adab5acc5da9454e0268fc9ab6fd
OliverTheLove <oliver.nyholm@gmail.com>
2018-09-06 17:11:39 +0200

    Added a description for spell! Still in progress with text

---
+++ Assets/Scripts/ActionKey.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.EventSystems;
+
+public class ActionKey : MonoBehaviour, IPointerEnterHandler, IPointerExitHandler
+{
+
+    [SerializeField]
+    private Button myButton;
+    [SerializeField]
+    private GameObject myDescription;
+
+    public void OnPointerEnter(PointerEventData eventData)
+    {
+        myDescription.GetComponentInChildren<Image>().enabled = true;
+        myDescription.GetComponentInChildren<Text>().enabled = true;
+    }
+
+    public void OnPointerExit(PointerEventData eventData)
+    {
+        myDescription.GetComponentInChildren<Image>().enabled = false;
+        myDescription.GetComponentInChildren<Text>().enabled = false;
+    }
+
+    public void SetDescription(string aText)
+    {
+        myDescription.GetComponentInChildren<Text>().text = aText;
+    }
+}

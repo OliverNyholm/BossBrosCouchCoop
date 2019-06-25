0525fa075f0a3b5e9b9fa0bbb75528eee019fb47
Oliver Nyholm <oliver.nyholm@gmail.com>
2019-06-25 17:06:13 +0300

    Added GDD and tutorial image. Added start for a settings thingy if needed.

---
+++ Assets/Scripts/Data/SettingsReader.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.IO;
+
+public class SettingsReader : MonoBehaviour
+{
+    [System.Serializable]
+    private class PlayerSettings
+    {
+        float MasterVolume;
+    }
+
+    private void Awake()
+    {
+    }
+
+    private void ReadJson()
+    {
+        string filePath = Path.Combine(Application.streamingAssetsPath, "Settings.json");
+        if(File.Exists(filePath))
+        {
+            string dataAsJson = File.ReadAllText(filePath);
+            PlayerSettings settings = JsonUtility.FromJson<PlayerSettings>(dataAsJson);
+        }
+    }
+}

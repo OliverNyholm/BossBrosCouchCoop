14907ab2657ac4cbc8f1919b5c6e890fad573781
OliverTheLove <oliver.nyholm@gmail.com>
2021-01-10 14:38:53 +0100

    Minions spawn whirlpools again. Nerfed pirouette even more

---
+++ Assets/Scripts/Bosses/BalletBoss/BalletBossMinion.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BalletBossMinion : NPCComponent
+{
+    private WhirlpoolHandler myWhirlpoolHandler = null;
+
+    protected override void Awake()
+    {
+        base.Awake();
+        myWhirlpoolHandler = FindObjectOfType<WhirlpoolHandler>();
+    }
+
+    protected override void OnDeath()
+    {
+        base.OnDeath();
+
+        myWhirlpoolHandler.AddWhirlpool(this);
+    }
+}
